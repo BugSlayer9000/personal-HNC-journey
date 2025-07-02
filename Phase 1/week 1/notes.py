5aee0a37f4ebf6858c78b86e10612141a55d19e5
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-02 13:19:45 +0100

    added week 1 phase 1 files

---
+++ Phase 1/week 1/notes.py
@@ -0,0 +1,16 @@
+#  samod subhasha
+# This is the synatx overview for a class 
+
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    
+    def bark(self):
+        print(f"{self.name} says woof! ")
+
+# creating an object
+dog1 = Dog("Max", "Labrador")
+dog1.bark() # Max says woof! 
+
+

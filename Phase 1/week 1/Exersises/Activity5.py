e8f249d9019d540fbced9a77724bfe2cfcbc5d7d
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-02 17:29:18 +0100

    Activity 5 pASSED

--- Phase 1/week 1/Exersises/Activity5.py
+++ Phase 1/week 1/Exersises/Activity5.py
@@ -9,5 +9,25 @@
 
 # 🔁 Extend: Ask the user for values, then create a rectangle and print area/perimeter.
 
+class Rectangle:
+    def __init__(self, width:int, height:int):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        area = self.width * self.height
+        return area
 
+    def perimeter(self):
+        perimeter = 2 * (self.height + self.width)
+        return perimeter
+
+def main():
+    rectangle = Rectangle(6,5)
+    print(f"Area : {rectangle.area()}")
+    print(f"Perimeter : {rectangle.perimeter()}")
+
+if __name__ == "__main__":
+    main()
+    
 

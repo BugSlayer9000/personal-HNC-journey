d865b75ac17687284f3d3e607206175fc8bf83ef
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-02 18:19:53 +0100

    Activity 5 Extended version

---
+++ Phase 1/week 1/Exersises/Activity5Extended.py
@@ -0,0 +1,36 @@
+# samod subhasha
+# 🔧 Activity 5 (Challenge): Rectangle Geometry
+
+# 🔁 Extend: Ask the user for values, then create a rectangle and print area/perimeter.
+
+class Rectangle:
+    def __init__(self):
+        self.get_height_and_width()
+    
+    def get_height_and_width(self):
+        try:
+            self.height = int(input("Enter the Height : "))
+            self.width = int(int(input("Enter the Width : ")))
+        except:
+            print("Invalid Input ! ")
+    
+    def area(self):
+        area = self.width * self.height
+        return area
+
+    def perimeter(self):
+        perimeter = 2 * (self.height + self.width)
+        return perimeter
+
+def main():
+    rectangle = Rectangle()
+    try:
+        print(f"Area : {rectangle.area()}")
+        print(f"Perimeter : {rectangle.perimeter()}")
+    except:
+        print("Error")
+
+if __name__ == "__main__":
+    main()
+    
+

23d1102a22dbfc4df3a78399b276832c63cd71c0
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-09 11:56:01 +0100

    Implement initial classes for deposit, withdraw and get balance

--- Phase 1/week 2/Exercises/exersice1.py
+++ Phase 1/week 2/Exercises/exersice1.py
@@ -10,3 +10,18 @@
 
 # Enforce that balance cannot go below 0 and deposit/withdraw must be positive.
 
+
+class BankAccount:
+    def __init__(self, balance, account_holder ):
+        self.__balance = balance
+        self.__account_holder = account_holder
+        
+    def deposit(self, amount):
+        pass
+    
+    def withdraw(self, amount):
+        pass
+    
+    def get_balance():
+        pass
+    

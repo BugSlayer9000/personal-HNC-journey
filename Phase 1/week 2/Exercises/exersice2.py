b2ab4b3326cf653ba1a6bb0123629060179f0594
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-09 14:05:28 +0100

    Initial information added for exercise 2

---
+++ Phase 1/week 2/Exercises/exersice2.py
@@ -0,0 +1,14 @@
+# ✅ Exercise 2: Student Record System
+# Task:
+# Create a Student class:
+
+# Private attributes: __name, __grades (list of ints)
+
+# Methods:
+
+# add_grade(grade) – Only allow 0–100.
+
+# average_grade() – Return average.
+
+# get_name() – Return name.
+

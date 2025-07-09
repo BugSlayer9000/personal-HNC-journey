a8af03d3658842b7db3323ad6e47f003cdaf7c53
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-09 11:51:36 +0100

    initial instruction Exercise 1 has been added

---
+++ Phase 1/week 2/Exercises/exersice1.py
@@ -0,0 +1,12 @@
+# samod subhasha 
+
+# ✅ Exercise 1: Bank Account Class
+# Task:
+# Create a BankAccount class with the following:
+
+# Private attributes: __balance, __account_holder
+
+# Methods: deposit(amount), withdraw(amount), get_balance()
+
+# Enforce that balance cannot go below 0 and deposit/withdraw must be positive.
+

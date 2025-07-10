4ffea9cbf87c471a622ea6e205273f4ddf76e923
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-10 12:30:05 +0100

    exercise 5 started made the initial instructions and initial code structure

---
+++ Phase 1/week 2/Exercises/exercise5.py
@@ -0,0 +1,36 @@
+# samod subhasha
+# ✅ Exercise 5: Inventory Item
+# Task:
+# Create an InventoryItem class:
+
+# Private attributes: __name, __price, __stock
+
+# Methods:
+
+# restock(amount)
+
+# sell(quantity)
+
+# get_stock()
+
+# get_details() – return a formatted string
+
+
+class Inventoryitem:
+    def __init__(self, name, price, stock):
+        self.__name = name
+        self.__price = price
+        self.__stock = stock
+    
+    def restock(self, amount):
+        pass
+    
+    def sell(quantitiy):
+        pass
+    
+    def get_stock(self):
+        pass
+    
+    def get_details(self):
+        pass
+    

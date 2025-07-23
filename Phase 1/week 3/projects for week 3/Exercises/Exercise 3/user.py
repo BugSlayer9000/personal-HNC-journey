94d5e6a194b6355de9fa19791bc51d1f470a1427
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-23 18:50:15 +0100

    added password hashing and create task permissions

--- Phase 1/week 3/projects for week 3/Exercises/Exercise 3/user.py
+++ Phase 1/week 3/projects for week 3/Exercises/Exercise 3/user.py
@@ -1,16 +1,26 @@
 from abc import abstractmethod, ABC
 import hashlib # Use Case: Password hashing for secure user credentials
+from enum import Enum
+
+class UserRoles(Enum):
+    ADMIN = "Admin"
+    MANAGER = "Manager"
+    EMPLOYEE = "Employeee"
 
 class User(ABC):
     def __init__(self, user_name:str, email:str, password_hash:str ) -> None:
         self.user_name = user_name
         self.email = email
-        self._password_hash = password_hash # hashed password
+        self._password_hash = self.hash_password(password_hash) # hashed password
+
+    def hash_password(self, password:str) -> str:
+        return hashlib.sha256(password.encode("utf-8")).hexdigest()
     
-    def check_password(self, password): # validates password input
-        pass
+    def check_password(self, input_password) -> bool: # validates password input
+        return self.hash_password(input_password) == self._password_hash
     
-    def get_role(self): # Returns your role
+    @abstractmethod
+    def get_role(self) -> str: # Returns your role
         pass
     
     def is_authorized(self, action):#
@@ -24,10 +34,24 @@ class User(ABC):
     def can_assign_task(self) -> bool:
         pass
     
+    @abstractmethod
+    def can_delete_task(self) -> bool:
+        pass
+
+
+
+
 class Admin(User): # full permissions
     def __init__(self, user_name: str, email: str, password_hash:str) -> None:
         super().__init__(user_name, email, password_hash)
-        
+        self.role = UserRoles.ADMIN.value
+
+    def get_role(self) -> str:
+        return f"{self.role}"
+    
+    def can_delete_task(self) -> bool:
+        return True
+    
     def can_assign_task(self) -> bool:
         return True
     
@@ -37,6 +61,13 @@ class Admin(User): # full permissions
 class Manager(User): # Can assign items to employees
     def __init__(self, user_name: str, email: str, password_hash:str) -> None:
         super().__init__(user_name, email, password_hash)
+        self.role = UserRoles.MANAGER.value
+
+    def get_role(self) -> str:
+        return f"{self.role}"
+    
+    def can_delete_task(self) -> bool:
+        return False
     
     def can_assign_task(self) -> bool:
         return True
@@ -47,9 +78,21 @@ class Manager(User): # Can assign items to employees
 class Employee(User): # can work on the assigned work
     def __init__(self, user_name: str, email: str, password_hash:str) -> None:
         super().__init__(user_name, email, password_hash)
+        self.role = UserRoles.EMPLOYEE.value
+    
+    def get_role(self) -> str:
+        return f"{self.role}"
     
     def can_assign_task(self) -> bool:
+        return False
+
+    def can_delete_task(self) -> bool:
         return False
     
     def can_create_task(self) -> bool:
         return False
+    
+    
+admin1 = Admin("samod", "samod@gmail.com", "pakaya")
+print(admin1.check_password("pakaya"))
+print(admin1.get_role())

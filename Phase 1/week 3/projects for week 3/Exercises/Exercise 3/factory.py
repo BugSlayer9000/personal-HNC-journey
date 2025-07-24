29ce7a9b10bec47ed687b5f848034b94395f9031
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-07-24 10:51:27 +0100

    update

--- Phase 1/week 3/projects for week 3/Exercises/Exercise 3/factory.py
+++
@@ -1,8 +0,0 @@
-
-
-class TaskFactory:
-    # ✔ Applies Factory Pattern
-    # ✔ Ensures tasks are validated on creation
-    
-    def create_task(self): # Creates and returns a task instance
-        pass

3295abb2f755f684033336cff277f864fdb43224
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-08-13 16:04:11 +0100

    added perishable item class

---
+++ Phase 1/week 6 Final/inventory app/items/perishable-item.py
@@ -0,0 +1,22 @@
+from item import Item
+from datetime import datetime
+
+class PerishableItem(Item):
+    def __init__(self, _id: str, _name: str, _price: float, _quantity: int, _category: str, _created_at: datetime, _expiry_date:datetime) -> None:
+        self._expiry_date = _expiry_date 
+        super().__init__(_id, _name, _price, _quantity, _category, _created_at)
+        
+        
+    
+    def _attribute_check(self):
+        if not isinstance(self._expiry_date, datetime):
+            raise ValueError("Expiry date must be an instance of datetime")
+        return super()._attribute_check()
+
+    @property
+    def expiry_date(self):
+        return self._expiry_date
+    
+
+
+

68595c5e2221690c7049fb0828b3b458b1062c2b
college commits <you@example.com>
2025-09-16 14:35:22 +0100

    - [x]  initialize the file paths - [x]  make sure they exist before proceed - [x]  initialize the file structure for CSV and JSON - [x]  add the save file method and load file method for CSV

---
+++ Phase 2/week 1/File Handling/practice/exercise1/file_handling.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import csv
+import json
+
+# csv file structure will be a list of dicts = [{'name': 'samod', 'subject': 'science', 'score': 20}, {'name': 'robert', 'subject': 'maths', 'score': 40}]
+
+# json file structure will include two more items with 
+
+
+class FileHandlingCSV():
+    student_details_csv = Path("Phase 2\\week 1\\File Handling\\practice\\exercise1\\files\\student_details.csv")
+    student_details_csv.touch(exist_ok=True)
+    
+    def __init__(self) -> None:
+        self._initialize_file()
+    
+    def _initialize_file(self):
+        with open(self.student_details_csv, "w")as f:
+            writer = csv.writer(f)
+            writer.writerow(["name", "subject","score"])
+    
+    def _load_file(self):
+        with open(self.student_details_csv, "r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            return reader
+    
+    def _save_file(self, name, subject, score):
+        with open(self.student_details_csv , "a", newline="")as f:
+            writer = csv.writer(f)
+            writer.writerow([name, subject, score])
+    
+    def csv_to_dict(self):
+        with open(self.student_details_csv, "r") as f:
+            return csv.DictReader(f)
+           
+    
+    
+class FileHandlingJSON():
+    student_details_with_marks_json = Path("Phase 2\\week 1\\File Handling\\practice\\exercise1\\files\\student_details_with_marks.json")
+    student_details_with_marks_json.touch(exist_ok=True)
+    
+    def __init__(self) -> None:
+        self._initialize_file()
+    
+    def  _initialize_file(self):
+        with open(self.student_details_with_marks_json,"w") as f:
+            json.dump([], f, indent=4)
+    
+    def _load_file(self):
+        pass
+    
+    def _save_file(self):
+        pass
+    
+    
+csv_handling  = FileHandlingCSV()
+json_handling = FileHandlingJSON() 
+    
+    
---
+++ Phase 2/week 1/File Handling/practice/exercise1/marks_analyzer.py
@@ -0,0 +1,14 @@
+# rogram 2: Student Results Analyzer (Level 7–8)
+
+# Read results.csv with name, subject, score.
+
+# Print average score for each student.
+
+# Identify the top performer.
+
+# put it in a json file 
+
+
+
+
+# a class to collect student detials and put it in a csv 

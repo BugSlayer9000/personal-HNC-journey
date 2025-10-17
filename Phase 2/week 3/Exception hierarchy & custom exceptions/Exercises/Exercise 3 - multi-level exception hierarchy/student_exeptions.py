b366a1e525d6d9c472e3d40cceab3461e2c05d0c
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-10-17 19:30:17 +0100

    Completed custom exeptions

--- Phase 2/week 3/Exception hierarchy & custom exceptions/Exercises/Exercise 3 - multi-level exception hierarchy/student_exeptions.py
+++ Phase 2/week 3/Exception hierarchy & custom exceptions/Exercises/Exercise 3 - multi-level exception hierarchy/student_exeptions.py
@@ -1,2 +1,44 @@
 # all custom exeptions here
 
+class StudentSystemError(Exception):
+    """Base for all student system errors"""
+    pass
+
+class InvalidStudentIDError(StudentSystemError):
+    """Raised when student ID fromat is Invalid (Must be 6 digits)"""
+    def __init__(self) -> None:
+        massage = f"The ID format is invalid must be 8 digits ! "
+        super().__init__(massage)
+
+class DuplicateStudentError(StudentSystemError):
+    """Raised when tried to add a student that's already exists"""
+    def __init__(self,student_id:int) -> None:
+        massage = f"A student under the if of - {student_id} Already exists in the system"
+        super().__init__(massage)
+
+class StudentNotFoundError(StudentSystemError):
+    """Raised when student id doesn't exist in the system"""
+    def __init__(self) -> None:
+        massage = f"Student ID not found in the system !  "
+        super().__init__(massage)
+
+
+
+
+class GradeError(Exception):
+    """Base for all gread relataed errors"""
+    pass
+
+class GradeOutOfRangeError(GradeError):
+    """Raised when grade is not between 0-100"""
+    
+    def __init__(self) -> None:
+        massage = f"The grade should be between 0-100 marks ! "
+        super().__init__(massage)
+
+class InvalidGradeTypeError(GradeError):
+    """Raised when grade is not a number """
+    
+    def __init__(self) -> None:
+        massage = f"The Grade must be a number !  "
+        super().__init__(massage)
